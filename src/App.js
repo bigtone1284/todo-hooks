7eb3e807ff1e22f9364fc4d78b48a66684bb69f9
Tony DeFreitas <anthony.defreitas@gmail.com>
2019-05-01 21:58:57 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import TodosContainer from './TodosContainer';
 import TodoForm from './TodoForm';
 import './App.css';
 
+const todoReducer = (state, action) => {
+  const prevTodos = state.todos;
+  const { task, idx, doneStatus, type } = action;
+
+  switch (type) {
+    case 'CREATE':
+      
+      return { ...state, todos: [{ task, done: false }, ...prevTodos] };
+    case 'TOGGLE_DONE':
+      prevTodos[idx].done = doneStatus;
+
+      return { ...state, todos: [...prevTodos] };
+    case 'DELETE':
+      prevTodos.splice(idx, 1);
+
+      return { ...state, todos: [...prevTodos] };
+    default:
+      throw new Error();
+  }
+};
+
 export default ({ initTodos }) => {
-  const [todos, setTodos] = useState(initTodos);
+  const [state, dispatch] = useReducer(todoReducer, { todos: initTodos });
 
-  const createTodo = (task) => setTodos(prevTodos => [
-    { task, done: false }, ...prevTodos
-  ]);
 
-  const toggleDone = (idx, doneStatus) => setTodos((prevTodos) => {
-    prevTodos[idx].done = doneStatus;
+  const createTodo = (task) => {
+    dispatch({ type: 'CREATE', task });
+  };
 
-    return [...prevTodos];
-  });
+  const toggleDone = (idx, doneStatus) => {
+    dispatch({ type: 'TOGGLE_DONE', idx, doneStatus });
+  };
 
-  const deleteTodo = (idx) => setTodos((prevTodos) => {
-    prevTodos.splice(idx, 1);
-
-    return [...prevTodos];
-  });
+  const deleteTodo = (idx) => {
+    dispatch({ type: 'DELETE', idx });
+  };
   
   return (
     <div className="App">
       <TodoForm onSubmit={createTodo} />
-      <TodosContainer todos={todos} deleteTodo={deleteTodo} toggleDone={toggleDone} />
+      <TodosContainer todos={state.todos} deleteTodo={deleteTodo} toggleDone={toggleDone} />
     </div>
   );
 }
