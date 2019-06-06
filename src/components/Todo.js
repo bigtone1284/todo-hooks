57f9ecd41730aeb38f13b5a797d636111b8afa92
Tony DeFreitas <anthony.defreitas@gmail.com>
2019-06-06 09:16:26 -0700

    refactor around material ui

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Mutation } from 'react-apollo'
 import LaunchIcon from '@material-ui/icons/Launch';
 import IconButton from '@material-ui/core/IconButton';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
 
 import '../styles/Todo.css';
 import { DELETE_TODO, TODOES_QUERY, UPDATE_TODO } from '../utils/queries';
@@ -19,6 +23,12 @@ const removeTodoFromCache = (cache, { data: { deleteTodo }}) => {
   });
 };
 
+const useStyles = (makeStyles({
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+}));
 
 
 export default ({ task, done, id }) => {
@@ -28,51 +38,55 @@ export default ({ task, done, id }) => {
 
   return (
     <li className="todo">
-      <div className="todo-launch-icon">
-        <IconButton color="primary" size="small" onClick={() => {
-          history.push(`/todos/${id}`)
-        }}>
-          <LaunchIcon fontSize="small" />
-        </IconButton>
-      </div>
-      <p className={'toto-header ' + (done ? 'strikethrough' : '')}>{task}</p>
-      <div className="button-container">
-        <Mutation mutation={UPDATE_TODO}>
-          { (updateTodo) => (
-            <LoadingButton
-              disabled={isDisabled}
-              isLoading={updateLoading}
-              color={done ? 'blue' : 'green'}
-              onClick={() => {
-                setUpdateLoading(true);
-                updateTodo({ variables: { id, done: !done } })
-                  .then(() => {
-                    setUpdateLoading(false);
-                  });
-              }}
-              btnText={done ? 'Undo' : 'Mark as Done'}
-            />
-          )}
-        </Mutation>
-        <Mutation mutation={DELETE_TODO} update={removeTodoFromCache}>
-          {
-            (deleteTodo) => (
-              done ? (
-                <LoadingButton
+      <Card>
+        <CardContent>
+          <div className="todo-launch-icon">
+            <IconButton color="primary" size="small" onClick={() => {
+              history.push(`/todos/${id}`)
+            }}>
+              <LaunchIcon fontSize="small" />
+            </IconButton>
+          </div>
+          <p className={'toto-header ' + (done ? 'strikethrough' : '')}>{task}</p>
+        </CardContent>
+        <CardActions className={useStyles().cardActions}>
+          <Mutation mutation={UPDATE_TODO}>
+            { (updateTodo) => (
+              <LoadingButton
                 disabled={isDisabled}
-                isLoading={deleteLoading}
-                color="red"
-                btnTxt="Delete"
+                isLoading={updateLoading}
+                color={done ? 'blue' : 'green'}
                 onClick={() => {
-                  setDeleteLoading(true);
-                  deleteTodo({ variables: { id } });
+                  setUpdateLoading(true);
+                  updateTodo({ variables: { id, done: !done } })
+                    .then(() => {
+                      setUpdateLoading(false);
+                    });
                 }}
-                btnText='Delete'
+                btnText={done ? 'Undo' : 'Mark as Done'}
               />
-            ) : (''))
-          }
-        </Mutation>
-      </div>
+            )}
+          </Mutation>
+          <Mutation mutation={DELETE_TODO} update={removeTodoFromCache}>
+            {
+              (deleteTodo) => (
+                done ? (
+                  <LoadingButton
+                  disabled={isDisabled}
+                  isLoading={deleteLoading}
+                  color="red"
+                  btnTxt="Delete"
+                  onClick={() => {
+                    setDeleteLoading(true);
+                    deleteTodo({ variables: { id } });
+                  }}
+                  btnText='Delete'
+                />
+              ) : (''))
+            }
+          </Mutation>
+        </CardActions>
+      </Card>
     </li>
   );
 }
