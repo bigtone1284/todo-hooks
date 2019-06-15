8fc1aff096395a8aa6f650909ff29722c7a0b51e
Tony DeFreitas <anthony.defreitas@gmail.com>
2019-06-15 11:26:02 -0700

    moved to todo app

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,17 @@
-import React, { useEffect, useReducer, useRef } from 'react';
+import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import history from '../utils/history';
 import TodoApp from './TodoApp';
 import TodoModal from './TodoModal';
 import '../styles/App.css';
-import todosReducer from '../reducers/todosReducer';
-import {
-  ADD_TODOS,
-  DELETE_TODO,
-  UPDATE_TODO
-} from '../actions/todosActions';
-import { createTodo, deleteTodo, fetchAllTodos, updateTodo } from '../utils/todoApi';
 
 const App = () => {
-  const [todos, dispatch] = useReducer(todosReducer, []);
-  const todoRef = useRef();
-
-  useEffect(() => {
-    fetchAllTodos()
-      .then(todos => {
-        dispatch({ type: ADD_TODOS, payload: { todos } });
-      })
-      .catch(err => err);
-  }, [todoRef]);
-
-  const handleCreateTodo = (task) => {
-    return createTodo(task)
-      .then(todo => {
-        dispatch({
-          type: ADD_TODOS,
-          payload: { todos: [todo] }
-        });
-
-        return !!todo;
-      });
-  };
-
-  const handleDeleteTodo = (deleteTodoId) => {
-    return deleteTodo(deleteTodoId)
-      .then(() => {
-          dispatch({
-            type: DELETE_TODO,
-            payload: {
-              deleteTodoId
-            }
-          });
-      });
-  };
-
-  const handleUpdateTodo = (updateTodoId, updateTodoData) => {
-    return updateTodo(updateTodoId, updateTodoData)
-      .then(updatedTodo => {
-        dispatch({
-          type: UPDATE_TODO,
-          payload: {
-            updatedTodo
-          }
-        });
-      });
-  };
+  
 
   return (
     <Router history={history}>
-      <div ref={todoRef} className="App">
-        <Route path="/" render={() => (
-          <TodoApp 
-            createTodo={handleCreateTodo}
-            deleteTodo={handleDeleteTodo}
-            todos={todos}
-            updateTodo={handleUpdateTodo}
-          />
-        )} />
+      <div className="App">
+        <Route path="/" component={TodoApp} />
         <Route path="/todos/:id" component={TodoModal} />
       </div>
     </Router>
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,15 +1,69 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer, useRef }from 'react';
 
 import TodosContainer from './TodosContainer';
 import TodoForm from './TodoForm';
+import todosReducer from '../reducers/todosReducer';
+import {
+  ADD_TODOS,
+  DELETE_TODO,
+  UPDATE_TODO
+} from '../actions/todosActions';
+import { createTodo, deleteTodo, fetchAllTodos, updateTodo } from '../utils/todoApi';
 
-export default ({ createTodo, deleteTodo, todos, updateTodo }) => {
+export default () => {
+  const [todos, dispatch] = useReducer(todosReducer, []);
+  const todoRef = useRef();
+
+  useEffect(() => {
+    fetchAllTodos()
+      .then(todos => {
+        dispatch({ type: ADD_TODOS, payload: { todos } });
+      })
+      .catch(err => err);
+  }, [todoRef]);
+
+  const handleCreateTodo = (task) => {
+    return createTodo(task)
+      .then(todo => {
+        dispatch({
+          type: ADD_TODOS,
+          payload: { todos: [todo] }
+        });
+
+        return !!todo;
+      });
+  };
+
+  const handleDeleteTodo = (deleteTodoId) => {
+    return deleteTodo(deleteTodoId)
+      .then(() => {
+          dispatch({
+            type: DELETE_TODO,
+            payload: {
+              deleteTodoId
+            }
+          });
+      });
+  };
+
+  const handleUpdateTodo = (updateTodoId, updateTodoData) => {
+    return updateTodo(updateTodoId, updateTodoData)
+      .then(updatedTodo => {
+        dispatch({
+          type: UPDATE_TODO,
+          payload: {
+            updatedTodo
+          }
+        });
+      });
+  };
+  
   return (
-    <Fragment>
-      <TodoForm createTodo={createTodo} />
+    <Fragment ref={todoRef}>
+      <TodoForm createTodo={handleCreateTodo} />
       <TodosContainer
-        deleteTodo={deleteTodo}
-        updateTodo={updateTodo}
+        deleteTodo={handleDeleteTodo}
+        updateTodo={handleUpdateTodo}
         todos={todos}
       />
     </Fragment>
